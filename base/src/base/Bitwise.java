fd9881c827e7c1519df2908e040c043ec8d6a113
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-19 06:20:24 -0300

    operadores bitwise

---
+++ base/src/base/Bitwise.java
@@ -0,0 +1,16 @@
+package base;
+
+public class Bitwise {
+
+	public static void main(String[] args) {
+		
+		int numeroUm = 89;
+		int numeroDois = 60;
+		
+		System.out.println(numeroUm & numeroDois);  // AND
+		System.out.println(numeroUm | numeroDois);  // OR
+		System.out.println(numeroUm ^ numeroDois);  // XOR
+		
+	}
+
+}
