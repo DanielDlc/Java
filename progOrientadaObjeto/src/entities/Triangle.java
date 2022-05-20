010ed1870d0b10e1d7a0afb27293c44fd419b980
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-20 15:14:09 -0300

    calcular a área de um triângulo com a classe Triangle

--- progOrientadaObjeto/src/entities/Triangle.java
+++ progOrientadaObjeto/src/entities/Triangle.java
@@ -6,4 +6,10 @@ public class Triangle {
 	public double b;
 	public double c;
 	
+	// Area of triangle formula >>>  area = sqrt p(p-a)(p-b)(p-c) where p=(a+b+C)/2	
+	public double area() {
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
+	
 }
