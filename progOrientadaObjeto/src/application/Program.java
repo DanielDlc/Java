d9c8a557346c13daa3c20909e86366fa66e5a0c7
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-20 07:27:47 -0300

    importação da classe Triangle, instanciando o objeto e acessando os atributos a, b e c.

--- progOrientadaObjeto/src/application/Program.java
+++ progOrientadaObjeto/src/application/Program.java
@@ -2,30 +2,36 @@ package application;
 
 import java.util.Scanner;
 
+import entities.Triangle;
+
+
 public class Program {
 
 	// Area of a triangle
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-		double xA, xB, xC, yA, yB, yC;
+
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
 		
 		System.out.println("Enter the measures of triangle X: ");
-		xA = input.nextDouble();
-		xB = input.nextDouble();
-		xC = input.nextDouble();
+		x.a = input.nextDouble();
+		x.b = input.nextDouble();
+		x.c = input.nextDouble();
 		
 		System.out.println("Enter the measures of triangle Y: ");
-		yA = input.nextDouble();
-		yB = input.nextDouble();
-		yC = input.nextDouble();
+		y.a = input.nextDouble();
+		y.b = input.nextDouble();
+		y.c = input.nextDouble();
 		
 		// Area of triangle formula >>>  area = sqrt p(p-a)(p-b)(p-c) where p=(a+b+C)/2		
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * (p -xB ) * (p - xC));
+		double p = (x.a + x.b + x.c) / 2.0;
+		double areaX = Math.sqrt(p * (p - x.a) * (p -x.b ) * (p - x.c));
 		
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA) * (p -yB ) * (p - yC));
+		p = (y.a + y.b + y.c) / 2.0;
+		double areaY = Math.sqrt(p * (p - y.a) * (p -y.b) * (p - y.c));
 		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
