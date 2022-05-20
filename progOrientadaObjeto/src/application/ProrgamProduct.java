e75e95827f1ce9a0e40ac2ccebbb3b5af74906a5
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-20 15:12:22 -0300

    calcular entrada e saída de um produto em estoque

---
+++ progOrientadaObjeto/src/application/ProrgamProduct.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Scanner;
+import entities.Product;
+
+public class ProrgamProduct {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		product.name = input.nextLine();
+		
+		System.out.print("Price: ");
+		product.price = input.nextDouble();
+		
+		System.out.println("Quantity in stock: ");		
+		product.quantity = input.nextInt();
+		
+		// imprimir para testar apenas.
+		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+		
+		input.close();
+
+	}
+
+}
