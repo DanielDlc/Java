2f8a8e829fd07dde0a16c5f719d2da3a4b02bce1
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-21 08:47:08 -0300

    programa modificado com classe separada calculator

--- progOrientadaObjeto/src/application/ProgCircumference.java
+++ progOrientadaObjeto/src/application/ProgCircumference.java
@@ -3,36 +3,31 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import progOrientadaObjeto.Calculator;
+
 public class ProgCircumference {
 	
-	public static final double PI = 3.14159;
-
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
 		
+		Calculator calc = new Calculator();
+		
 		System.out.println("Enter radius: ");
 		double radius = input.nextDouble();
 		
-		double c = circumference(radius);
+		double c = calc.circumference(radius);
 		
-		double v = volume(radius);
+		double v = calc.volume(radius);
 		
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI value: %.2f%n", PI);
+		System.out.printf("PI value: %.2f%n", calc.PI);
 		
 		
 		input.close();
 
 	}
 	
-	public static double circumference(double radius) {
-		return 2.0 * PI * radius;
-	}
-	
-	public static double volume(double radius) {
-		return 4 * PI * radius * radius * radius / 3.0;
-	}
 }
