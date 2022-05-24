e92fcacdda50b0fea5b397662d2e2164044da9d5
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-24 07:24:38 -0300

    exibindo data e hora no padrão ISO8601

---
+++ trabalhandoComDatas/src/trabalhandoComDatas/Calendar.java
@@ -0,0 +1,19 @@
+package trabalhandoComDatas;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date; 
+
+public class Calendar {
+
+	public static void main(String[] args) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		Date data = Date.from(Instant.parse("2022-05-24T07:10:50Z"));
+		
+		System.out.println(sdf.format(data));		
+
+	}
+
+}
