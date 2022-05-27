11be36d7341e524bfa4addd5d4657a02fa82119f
DanielDlc <dlc.engcomputacao@gmail.com>
2022-05-27 11:15:48 -0300

    exibindo data e hora com localDateTime

---
+++ trabalhandoComDatas/src/trabalhandoComDatas/DataeHora.java
@@ -0,0 +1,32 @@
+package time;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class DataeHora {
+	
+	public static void main(String[] args) {
+		
+		// exibir apenas a data
+		LocalDate localDate = LocalDate.now();
+		System.out.println("localDate: " + localDate);
+		
+		// exibir apenas a hora
+		LocalTime localTime = LocalTime.now();
+		System.out.println("localTime: " + localTime);
+		
+		//exibir date e hora
+		LocalDateTime localDateTime = LocalDateTime.now();
+		System.out.println("LocalDateTime: " + localDateTime);
+		
+		System.out.println("Dia: " + localDateTime.getDayOfMonth());
+		System.out.println("Mês: " + localDateTime.getMonth());
+		System.out.println("Ano: " + localDateTime.getYear());
+		System.out.println("Hora: " + localDateTime.getHour());
+		System.out.println("Minuto: " + localDateTime.getMinute());
+		System.out.println("Segundos: " + localDateTime.getSecond());
+		
+		
+	}
+}
