53003e10b31d5f33751c9c6278d9376f4b68933a
DanielDlc <dlc.engcomputacao@gmail.com>
2022-06-03 07:39:08 -0300

    efetuando saque de 200.0 com desconto de 5.0 da classe Account

--- progOrientadaObjHeran/src/application/Program.java
+++ progOrientadaObjHeran/src/application/Program.java
@@ -1,39 +1,17 @@
 package application;
 
 import entities.Account;
-import entities.BusinessAccount;
-import entities.SavingsAccount;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Account acc = new Account(1001, "Diego", 0.0);
-		BusinessAccount bacc = new BusinessAccount(1001, "MAria", 0.0, 500.0);
+		Account account = new Account(1001, "Jean", 1000.0);
+		account.withdraw(200.0);
+		System.out.println(account.getBalance());
+		
 
-		// UPCASTING 
-		Account acc1 = bacc;
-		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-		Account acc3 = new SavingsAccount(1004, "Miguel", 0.0, 0.01);
-		
-		// DOWNCASTING
-		BusinessAccount acc4 = (BusinessAccount)acc2;
-		acc4.loan(100.0);
-		
-		// BusinessAccount acc5 = (BusinessAccount)acc3;
-		if (acc3 instanceof BusinessAccount) {
-			BusinessAccount acc5 = (BusinessAccount)acc3;
-			acc5.loan(200.00);
-			System.out.println("Loan!");
-		}
-		
-		if (acc3 instanceof SavingsAccount) {
-			SavingsAccount acc5 = (SavingsAccount)acc3;
-			acc5.updateBalance();
-			System.out.println("Update!");
-		}
-		
-		
 	}
 
 }
