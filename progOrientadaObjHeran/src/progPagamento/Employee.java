3d67a969ef0cc6f7d15a916a827b2786f28f0755
DanielDlc <dlc.engcomputacao@gmail.com>
2022-06-08 10:24:25 -0300

    corrigindo Employee

--- progOrientadaObjHeran/src/progPagamento/Employee.java
+++ progOrientadaObjHeran/src/progPagamento/Employee.java
@@ -5,6 +5,7 @@ public class Employee {
 	private String name;
 	private Integer hours;
 	private Double valuePerHour;
+	
 	public Employee(String name, Integer hours, Double valuePerHour) {
 		this.name = name;
 		this.hours = hours;
