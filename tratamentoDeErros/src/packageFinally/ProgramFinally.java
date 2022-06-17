6883aaea89d21679edd28877ada3f2c122ca5bba
DanielDlc <dlc.engcomputacao@gmail.com>
2022-06-17 12:07:19 -0300

    classe finally

---
+++ tratamentoDeErros/src/packageFinally/ProgramFinally.java
@@ -0,0 +1,30 @@
+package packageFinally;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ProgramFinally {
+	
+	public static void main(String[] args) {
+		
+		File file = new File("C:\\Users\\sharm\\dev\\Java\\tratamentoDeErros\\src\\packageFinally\\in.txt");
+		Scanner input = null;
+		try {
+			input = new Scanner(file);
+			while (input.hasNextLine()) {
+				System.out.println(input.nextLine());
+			}
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		}
+		finally {
+			if (input != null) {
+				input.close();
+			}
+		}
+		
+	}
+
+}
